60d1e83c6ab377377a55cd7e789d7817edfc31e6
lu bowen <reginold.lu@gmail.com>
2022-01-31 00:58:51 +0900

    feat: add the login, login status, logout, signup

--- twitter_pj/twitter/urls.py
+++ twitter_pj/twitter/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from accounts.api.views import UserViewSet
+from accounts.api.views import AccountViewSet, UserViewSet
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r"api/users", UserViewSet)
+router.register(r"api/accounts", AccountViewSet, basename="accounts")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
