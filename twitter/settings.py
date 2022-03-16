2b47b4bd4e9fe054436b6eb52e9b8ad8d8e7d647
Bowen Lu <reginold.lu@gmail.com>
2022-03-16 09:22:47 +0000

    feat: change a little bit chore file

--- twitter/settings.py
+++ twitter/settings.py
@@ -134,7 +134,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-MEDIA_URL = "/media/"
-
-MEDIA_ROOT = "/vol/web/media"
-STATIC_ROOT = "/vol/web/static"
