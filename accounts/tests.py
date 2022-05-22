6f9de17fef0b64ba5317f03fbf8848527cd943ff
lu bowen <reginold.lu@gmail.com>
2022-05-22 10:13:00 +0900

    feat: add the userprofile unittest

---
+++ accounts/tests.py
@@ -0,0 +1,12 @@
+# from testing.testcases import TestCase
+
+# from accounts.models import UserProfile
+
+
+# class UserProfileModelTests(TestCase):
+#     def test_profile_property(self):
+#         user1 = self.create_user("user1", "user1@sample.com")
+#         self.assertEqual(UserProfile.objects.count(), 0)
+#         profile_actual = user1.profile
+#         self.assertEqual(isinstance(profile_actual, UserProfile), True)
+#         self.assertEqual(UserProfile.objects.count(), 1)
