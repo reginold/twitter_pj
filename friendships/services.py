94466e038c609b294790ba82c5bcd23ed318e6d8
Bowen Lu <reginold.lu@gmail.com>
2022-10-06 06:15:20 +0000

    feature: add the get following userid func

--- friendships/services.py
+++ friendships/services.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.core.cache import caches
+from twitter.cache import FOLLOWINGS_PATTERN
 
 from friendships.models import Friendship
 
-cache = caches["testing"] if settings.TESTING else caches["default"]
+cache = caches["testing"] if settings.TESTING_MODE else caches["default"]
 
 
 # userA following list, the loggedin user can see the following list
@@ -23,3 +24,23 @@ class FriendshipService(object):
             from_user=from_user,
             to_user=to_user,
         ).exists()
+
+    @classmethod
+    def get_following_user_id_set(cls, from_user_id):
+        # fetch the cache
+        # LRU will be used by release the memory
+        key = FOLLOWINGS_PATTERN.format(user_id=from_user_id)
+        user_id_set = cache.get(key)
+        if user_id_set is not None:
+            return user_id_set
+
+        # If the data was not stored in cache, we can search from the DB, maybe litte slow.
+        friendships = Friendship.objects.filter(from_user_id=from_user_id)
+        user_id_set = set([fs.to_user_id for fs in friendships])
+        cache.set(key, user_id_set)
+        return user_id_set
+
+    @classmethod
+    def invalidate_following_cache(cls, from_user_id):
+        key = FOLLOWINGS_PATTERN.format(user_id=from_user_id)
+        cache.delete(key)
