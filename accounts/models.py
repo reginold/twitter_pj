b3af6cecd420219869014574f4b9ada4faa9a65f
lu bowen <reginold.lu@gmail.com>
2022-12-22 00:49:20 +0900

    feat: add the listener and cache, modify the models

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save, pre_delete
+
+from accounts.listeners import profile_changed, user_changed
 
 
 class UserProfile(models.Model):
@@ -17,11 +20,20 @@ class UserProfile(models.Model):
 
 
 def get_profile(user):
+    # Void loop
+    from accounts.services import UserService
     if hasattr(user, "_cached_user_profile"):
         return getattr(user, "_cached_user_profile")
-    profile, _ = UserProfile.objects.get_or_create(user=user)
+    profile = UserService.get_profile_through_cache(user.id)
     setattr(user, "_cached_user_profile", profile)
     return profile
 
 
 User.profile = property(get_profile)
+
+# hook up with listeners to invalidate cache
+pre_delete.connect(user_changed, sender=User)
+post_save.connect(user_changed, sender=User)
+
+pre_delete.connect(profile_changed, sender=UserProfile)
+post_save.connect(profile_changed, sender=UserProfile)
