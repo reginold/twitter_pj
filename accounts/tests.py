654cfa6af24513eb3a19a34b8076a3b7166b0c5f
lu bowen <reginold.lu@gmail.com>
2022-12-23 01:12:38 +0900

    test: add the test about the cached

--- accounts/tests.py
+++ accounts/tests.py
@@ -7,7 +7,8 @@ class UserProfileTests(TestCase):
         self.clear_cache()
 
     def test_profile_property(self):
-        user1 = self.create_user("user1", "user1@sampl.com")
-        self.assertEqual(UserProfile.objects.count(), 3)
+        user1 = self.create_user("user1", "user1@sample.com")
+        self.assertEqual(UserProfile.objects.count(), 0)
         profile = user1.profile
         self.assertEqual(isinstance(profile, UserProfile), True)
+        self.assertEqual(UserProfile.objects.count(), 1)
