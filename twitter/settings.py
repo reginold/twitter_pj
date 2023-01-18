1f02de03b5460bb70d3221c6f1308e4b64c998ba
Bowen Lu <reginold.lu@gmail.com>
2023-01-18 14:44:35 +0000

    setting: add the redis setting

--- twitter/settings.py
+++ twitter/settings.py
@@ -182,6 +182,14 @@ CACHES = {
     },
 }
 
+# Install the Redis
+# add the line into the Dockerfile: apt-get install redis
+# add the redis python in requirement, pip install redis
+REDIS_HOST = "127.0.0.1"
+REDIS_PORT = 6379
+REDIS_DB = 0 if TESTING_MODE else 1
+REDIS_KEY_EXPIRE_TIME = 7 * 86400  # in seconds
+
 try:
     from .local_settings import *
 except:
