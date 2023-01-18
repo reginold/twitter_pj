159942eb45f0746feab854113b029048cfd3891b
Bowen Lu <reginold.lu@gmail.com>
2023-01-18 14:44:50 +0000

    test: add the redis unittest

--- tweets/tests.py
+++ tweets/tests.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 from testing.testcases import TestCase
 from tweets.constants import TweetPhotoStatus
 from tweets.models import Tweet, TweetPhoto
+from utils.redis_client import RedisClient
+from utils.redis_serializers import DjangoModelSerializer
 from utils.time_helpers import utc_now
 
 
@@ -41,3 +43,15 @@ class TweetTests(TestCase):
         self.assertEqual(photo.user, self.user1)
         self.assertEqual(photo.status, TweetPhotoStatus.PENDING)
         self.assertEqual(self.tweet.tweetphoto_set.count(), 1)
+
+    def test_cache_tweet_in_redis(self):
+        tweet = self.create_tweet(self.user1)
+        conn = RedisClient.get_connection()
+        serialized_data = DjangoModelSerializer.serialize(tweet)
+        conn.set(f"tweet: {tweet.id}", serialized_data)
+        data = conn.get(f"tweet:not_exists")
+        self.assertEqual(data, None)
+
+        data = conn.get(f"tweet: {tweet.id}")
+        cached_tweet = DjangoModelSerializer.deserializer(data)
+        self.assertEqual(tweet, cached_tweet)
