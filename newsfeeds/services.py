3d1de83125dc0bc827a07748b3a1fb59ee76c278
lu bowen <reginold.lu@gmail.com>
2023-07-22 21:54:34 +0900

    feat: add the celery fanout refactor

--- newsfeeds/services.py
+++ newsfeeds/services.py
@@ -1,5 +1,5 @@
 from newsfeeds.models import NewsFeed
-from newsfeeds.tasks import fanout_newsfeeds_task
+from newsfeeds.tasks import fanout_newsfeeds_main_task
 from twitter.cache import USER_NEWSFEEDS_PATTERN
 from utils.redis_helper import RedisHelper
 
@@ -7,7 +7,7 @@ from utils.redis_helper import RedisHelper
 class NewsFeedServices:
     @classmethod
     def fanout_to_followers(cls, tweet):
-        fanout_newsfeeds_task.delay(tweet.id)
+        fanout_newsfeeds_main_task.delay(tweet.id, tweet.user_id)
 
     @classmethod
     def get_cached_newsfeeds(cls, user_id):
